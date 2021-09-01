05cfc1ea8f2f9136f02aaf1418d32f08ce45bed2
jensenmk <jensenmk@g.cofc.edu>
2021-09-01 15:47:49 -0400

    completed lab 1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,13 @@
+"""
+Name: <Mark Jensen>
+hello.py
+
+Problem: this program ensures proper environment setup
+
+Certification of Authenticity:
+I certify that this work is entirely my own
+"""
+
+
+def main():
+    print("hello, world!")
