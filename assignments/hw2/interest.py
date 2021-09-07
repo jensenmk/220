9a1183dc49a8c11229d0dd439de3b48943a69e15
jensenmk <jensenmk@g.cofc.edu>
2021-09-07 15:19:20 -0400

    completed HW2

--- assignments/hw2/interest.py
+++ assignments/hw2/interest.py
@@ -11,11 +11,13 @@ I certify that this work is entirely my own
 def main():
     rate = eval(input("annual interest rate: "))
     days = eval(input("number of days in billing cycle: "))
-    previousBalance = eval(input("previous net balance: "))
+    previous_balance = eval(input("previous net balance: "))
     payment = eval(input("payment amount: "))
-    paymentDay = eval(input("day payment received: "))
-    average_daily_balance = ((previousBalance * days) - (payment * (days - paymentDay))) / days
-    monthlyinterest = rate / 12 / 100 * average_daily_balance
-    print("monthly interest: $", round(monthlyinterest, 2))
+    payment_day = eval(input("day payment received: "))
+    average_daily_balance = ((previous_balance * days) - (payment * (days - payment_day))) / days
+    monthly_interest = rate / 12 / 100 * average_daily_balance
+    print(round(monthly_interest, 2))
 
-main()
+
+
+#main()
