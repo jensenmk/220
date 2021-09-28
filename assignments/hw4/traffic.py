65716530f17521e96f2850d7f0183597c4184c70
jensenmk <jensenmk@g.cofc.edu>
2021-09-28 17:32:50 -0400

    Completed HW 4

--- assignments/hw4/traffic.py
+++ assignments/hw4/traffic.py
@@ -8,21 +8,22 @@ Certificate of Authenticity: I certify that this work is entirely my own
 
 
 def main():
-    r_i = eval(input("How many roads were surveyed? "))
-    acc_2 = 0
-    for i in range(0, r_i):
-        d_i = eval(input("how many days was road " + str(i + 1) + " " + "surveyed? "))
+    roads = eval(input("How many roads were surveyed? "))
+    acc2 = 0
+    for i in range(roads):
+        days = eval(input("How many days was road " + str(i + 1) + " surveyed? "))
         acc = 0
-        for y_i in range(0, d_i):
-            num = eval(input("How many cars traveled on day " + str(y_i + 1) + "? "))
-            acc = acc + num
-        acc_2 = acc + acc_2
-        d_1avg = acc / d_i
-        print("road " + str(i) + " " + "average " + str(round(d_1avg, 2)))
+        for j in range(days):
+            num = eval(input("\tHow many cars traveled on day " + str(j + 1) + "? "))
+            acc = num + acc
+        daily_avg = acc / days
+        print("Road " + str(i + 1) + " average vehicles per day: " + str(round(daily_avg, 2)))
+        acc2 = acc + acc2
+    tot_avg = acc2 / roads
+    tot_cars = acc2
 
-    t_avr = acc_2 / r_i
-    print("Total number of vehicles traveled on all roads: " + str(acc_2))
-    print(("Average number of vehicles per road: " + str(round(t_avr, 2))))
+    print("Total number of vehicles traveled on all roads: " + str(tot_cars))
+    print("Average number of vehicles per road: " + str(round(tot_avg, 2)))
 
 
 if __name__ == "__main__":
