81e1d001b793ea60c3e0a9c4fc396330181f61ca
jensenmk <jensenmk@g.cofc.edu>
2021-11-15 14:40:17 -0500

    Completed lab 11

--- labs/lab11/lab11.py
+++ labs/lab11/lab11.py
@@ -1,11 +1,61 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Mark Jensen
+hangman.py
 """
 
+from random import randint
+
+
 def main():
-    # add other function calls here
+    play()
     pass
+
+
+def words(file_name):
+    in_file = open(file_name, 'r')
+    content = in_file.read()
+    return content.split('\n')
+
+
+def rand_word(word_list):
+    return word_list[randint(0, len(word_list)-1)]
+
+
+def fill(word, letters):
+    secret = ["_"] * len(word)
+    for letter in letters:
+        for i in range(len(word)):
+            if letter == word[i]:
+                secret[i] = letter
+    return ''.join(secret)
+
+
+def won(word, letters):
+    secret = fill(word, letters)
+    if secret == word:
+        return True
+    else:
+        return False
+
+
+def play():
+    word_list = words('wordlist.txt')
+    secret = rand_word(word_list)
+    incorrect = 0
+    guesses = []
+    while not won(secret, guesses) and incorrect <= 7:
+        display = fill(secret, guesses)
+        print(display)
+        print(guesses)
+        letter = input('enter guess: ')
+        while letter in guesses:
+            letter = input("enter a letter you haven't entered yet")
+        guesses.append(letter)
+        display = fill(secret, guesses)
+        if guesses != display:
+            incorrect += 1
+        else:
+            guesses = display
 
 
 main()
